eac6d3f92234092f11c3e3033a99fa683ba99ff5
Amarjeet5822 <amar.bst5822@gmail.com>
2024-12-10 09:45:04 +0530

    Added image in Public folder

---
+++ Ecommerce/src/App.jsx
@@ -0,0 +1,16 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h2 className="text-2xl text-center">Hello World!</h2>
+      </div>    
+    </>
+  )
+}
+
+export default App;
